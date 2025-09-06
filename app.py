30cb3be53139f50c9188965818236c83f684dd8d
Елизавета Булыгина <lbulygina01@mail.ru>
2025-09-06 10:55:11 +0700

    Информация об авторе

--- app.py
+++ app.py
@@ -2,10 +2,28 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def start():
+@app.route('/web')
+def web():
     return """<!doctype html>
-        <html> \
-           <body> \
-                <h1>Web-сервес на flask </h1> \
-           </body> \
+        <html> 
+           <body> 
+                <h1>Web-сервес на flask </h1> 
+                <a href="/author">author</a>
+           </body> 
+        </html>"""
+
+@app.route('/author')
+def author():
+    name = "Булыгина Елизавета Денисовна"
+    group = "ФБИ-34"
+    faculty = "ФБ"
+
+    return """<!doctype html>
+        <html> 
+           <body> 
+                <P>Студент: """ + name + """</p> 
+                <P>Группа: """ + group + """</p> 
+                <P>Факультет: """ + faculty+ """</p> 
+                <a href="/web">web</a>
+           </body> 
         </html>"""
