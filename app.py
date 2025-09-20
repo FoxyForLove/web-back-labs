8aff44036ec2bfb1079313a39d88472e7ef479bb
Елизавета Булыгина <lbulygina01@mail.ru>
2025-09-20 10:26:31 +0700

    Имя студента

--- app.py
+++ app.py
@@ -378,4 +378,5 @@ def add_flower(name):
 
 @app.route('/lab2/example')
 def example():
-    return render_template('example.html')
+    name = 'Елизавета Булыгина'
+    return render_template('example.html', name=name)
