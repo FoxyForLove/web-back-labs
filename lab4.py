d2e887464db4de6f140797a880140428f034a05c
Елизавета Булыгина <lbulygina01@mail.ru>
2025-10-31 16:49:07 +0700

    Валидация

--- lab4.py
+++ lab4.py
@@ -101,18 +101,19 @@ def pow_():
     return render_template('lab4/pow.html', x1=x1, x2=x2, result=result)
 
 tree_count = 0
+max_trees = 25
 
 @lab4.route("/lab4/tree", methods= ['GET', 'POST'])
 def tree():
-    global tree_count
+    global tree_count, max_trees
     if request.method == 'GET':
-        return render_template("lab4/tree.html", tree_count=tree_count)
+        return render_template("lab4/tree.html", tree_count=tree_count, max_trees=max_trees)
     
     operation = request.form.get('operation')
 
-    if operation == 'cut':
+    if operation == 'cut' and tree_count > 0:
         tree_count -= 1
-    elif operation == 'plant':
+    elif operation == 'plant' and tree_count < max_trees:
         tree_count += 1
 
     return redirect('/lab4/tree')
