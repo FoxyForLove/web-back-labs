14e2a988cff452c79b72a67c23ad38c2fe117757
Елизавета Булыгина <lbulygina01@mail.ru>
2025-10-31 16:10:14 +0700

    Арифметика

--- lab4.py
+++ lab4.py
@@ -6,9 +6,11 @@ lab4 = Blueprint('lab4',__name__)
 def lab():
     return render_template("lab4/lab4.html")
 
+
 @lab4.route("/lab4/div-form")
 def div_form():
     return render_template("lab4/div-form.html")
+
 
 @lab4.route("/lab4/div", methods = ['POST'])
 def div():
@@ -22,3 +24,78 @@ def div():
     x2 = int(x2)
     result = x1 / x2
     return render_template('lab4/div.html', x1=x1, x2=x2, result=result)
+
+
+@lab4.route("/lab4/sum-form")
+def sum_form():
+    return render_template("lab4/sum-form.html")
+
+
+@lab4.route("/lab4/sum", methods=['POST'])
+def summation():
+    x1 = request.form.get('x1')
+    x2 = request.form.get('x2')
+
+    if x1 == '':
+        x1 = '0'
+    if x2 == '':
+        x2 = '0'
+
+    result = int(x1) + int(x2)
+    return render_template('lab4/sum.html', x1=x1, x2=x2, result=result)
+
+
+@lab4.route("/lab4/mul-form")
+def mul_form():
+    return render_template("lab4/mul-form.html")
+
+
+@lab4.route("/lab4/mul", methods=['POST'])
+def mul():
+    x1 = request.form.get('x1')
+    x2 = request.form.get('x2')
+
+    if x1 == '':
+        x1 = '1'
+    if x2 == '':
+        x2 = '1'
+
+    result = int(x1) * int(x2)
+    return render_template('lab4/mul.html', x1=x1, x2=x2, result=result)
+
+
+@lab4.route("/lab4/sub-form")
+def sub_form():
+    return render_template("lab4/sub-form.html")
+
+
+@lab4.route("/lab4/sub", methods=['POST'])
+def sub():
+    x1 = request.form.get('x1')
+    x2 = request.form.get('x2')
+
+    if x1 == '' or x2 == '':
+        return render_template('lab4/sub.html', error='Оба поля должны быть заполнены!')
+
+    result = int(x1) - int(x2)
+    return render_template('lab4/sub.html', x1=x1, x2=x2, result=result)
+
+
+@lab4.route("/lab4/pow-form")
+def pow_form():
+    return render_template("lab4/pow-form.html")
+
+
+@lab4.route("/lab4/pow", methods=['POST'])
+def pow_():
+    x1 = request.form.get('x1')
+    x2 = request.form.get('x2')
+
+    if x1 == '' or x2 == '':
+        return render_template('lab4/pow.html', error='Оба поля должны быть заполнены!')
+
+    if x1 == '0' and x2 == '0':
+        return render_template('lab4/pow.html', error='0 в степени 0 не определено!')
+
+    result = int(x1) ** int(x2)
+    return render_template('lab4/pow.html', x1=x1, x2=x2, result=result)
