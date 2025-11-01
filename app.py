c2b07c29be56c9dc666d37b3016dd2e852cd437a
Елизавета Булыгина <lbulygina01@mail.ru>
2025-11-01 08:57:33 +0700

    Заготовка к 5-ой лабораторной

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from lab1 import lab1
 from lab2 import lab2
 from lab3 import lab3
 from lab4 import lab4
+from lab5 import lab5
 
 access_log = [] 
 
@@ -17,6 +18,7 @@ app.register_blueprint(lab1)
 app.register_blueprint(lab2)
 app.register_blueprint(lab3)
 app.register_blueprint(lab4)
+app.register_blueprint(lab5)
 
 @app.route('/')
 @app.route('/index')
