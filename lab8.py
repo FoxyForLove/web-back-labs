7e539679f3aacb2ba5702d07bfa277461665a4be
Елизавета Булыгина <lbulygina01@mail.ru>
2025-12-09 18:16:53 +0700

    Авторизация

--- lab8.py
+++ lab8.py
@@ -7,6 +7,7 @@ import sqlite3
 from os import path
 from db import db
 from db.models import users,articles
+from flask_login import login_user, login_required, current_user
 
 lab8 = Blueprint('lab8', __name__)
 
@@ -41,3 +42,36 @@ def register():
     db.session.add(new_user)
     db.session.commit()
     return redirect('/lab8/')
+
+
+@lab8.route('/lab8/login', methods = ['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('lab8/login.html')
+    
+    login_form = request.form.get('login')
+    password_form = request.form.get('password')
+
+    if not login_form:
+        return render_template('lab8/login.html',
+                               error='Логин не должен быть пустым')
+
+    if not password_form:
+        return render_template('lab8/login.html',
+                               error='Пароль не должен быть пустым')
+
+    user = users.query.filter_by(login = login_form).first()
+
+    if user:
+        if check_password_hash(user.password, password_form):
+            login_user(user, remember = False)
+            return redirect('/lab8/')
+        
+    return render_template('/lab8/login.html',
+                           error = 'Ошибка входа: логин и/или пароль неверны')
+
+
+@lab8.route('/lab8/articles/')
+@login_required
+def article_list():
+    return "Список статей"
