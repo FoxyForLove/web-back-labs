0800a601d1b027ed78d6c391c187e57b13c957e9
Елизавета Булыгина <lbulygina01@mail.ru>
2025-12-09 16:54:00 +0700

    Модель базы данных

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ import datetime
 from werkzeug.exceptions import HTTPException
 import sys
 import os
+from os import path
+from flask_sqlalchemy import SQLAlchemy
+from db import db
 
 from lab1 import lab1
 from lab2 import lab2
@@ -21,6 +24,21 @@ app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'Секретно-сек
 app.config['DB_TYPE'] = os.getenv('DB_TYPE', 'postgres')
 
 
+if app.config['DB_TYPE'] == 'postgres':
+    db_name = 'foxy_fox'
+    db_user = 'foxy_fox'
+    db_password = '1234'
+    host_ip = '127.0.0.1'
+    host_port = 5432
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = \
+        f'postgresql+psycopg2://{db_user}:{db_password}@{host_ip}:{host_port}/{db_name}'
+else:
+    dir_path = path.dirname(path.realpath(__file__))
+    db_path = path.join(dir_path, "foxy_fox.db")
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_path}'
+
+db.init_app(app)
 
 app.register_blueprint(lab1)
 app.register_blueprint(lab2)
