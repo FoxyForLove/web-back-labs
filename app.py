824d4e2d0534f0d16b852044e96a2442d674239c
Елизавета Булыгина <lbulygina01@mail.ru>
2025-12-09 21:10:59 +0700

    Заготовка к 9-ой лабораторной

--- app.py
+++ app.py
@@ -17,6 +17,7 @@ from lab5 import lab5
 from lab6 import lab6
 from lab7 import lab7
 from lab8 import lab8
+from lab9 import lab9
 
 access_log = [] 
 
@@ -60,6 +61,7 @@ app.register_blueprint(lab5)
 app.register_blueprint(lab6)
 app.register_blueprint(lab7)
 app.register_blueprint(lab8)
+app.register_blueprint(lab9)
 
 @app.route('/')
 @app.route('/index')
@@ -85,6 +87,7 @@ def index():
             <a href="/lab6">Шестая лабораторная</a>
             <a href="/lab7">Седьмая лабораторная</a>
             <a href="/lab8">Восьмая лабораторная</a>
+            <a href="/lab9">Девятая лабораторная</a>
         </nav>
 
         <footer>
